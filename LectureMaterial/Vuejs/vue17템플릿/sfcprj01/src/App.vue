b4b8d01d9eeba5c5083797b414a52e908faf1902
eomjeonga <dmsgud4249@gmail.com>
2022-11-13 18:10:32 +0900

    20221113_end

---
+++ LectureMaterial/Vuejs/vue17템플릿/sfcprj01/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    App
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      
+    };
+  },
+  components:{
+
+  },
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
+
+vue
